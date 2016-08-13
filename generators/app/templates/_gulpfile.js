536f8a37b169200b236731e6ddc625f9d265a9b6
Benny Neugebauer <bn@bennyn.de>
2016-08-13 12:35:53 +0200

    Babel preset without 'strict' mode

--- generators/app/templates/_gulpfile.js
+++ generators/app/templates/_gulpfile.js
@@ -1,9 +1,12 @@
 var assets = require('gulp-bower-assets');
+var babel = require('gulp-babel');
 var bower = require('gulp-bower');
 var browserSync = require('browser-sync').create();
+var concat = require('gulp-concat');
 var gulp = require('gulp');
+var pkg = require('./package.json');
 var Server = require('karma').Server;
-var babel = require('gulp-babel');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('build', function() {
   return gulp.src('src/main/js/**/*.js')
@@ -13,12 +16,22 @@ gulp.task('build', function() {
 
 gulp.task('default', function() {
   gulp.watch('public/**/*.*').on('change', browserSync.reload);
+  gulp.watch('src/main/js/**/*.*', ['build']);
 
   browserSync.init({
     port: 3636,
     server: {baseDir: './'},
     startPath: '/public'
   });
+});
+
+gulp.task('dist', function() {
+  return gulp.src('src/main/js/**/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(babel())
+    .pipe(concat(pkg.name + '.js'))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('install', ['install_bower_assets'], function() {
@@ -34,7 +47,13 @@ gulp.task('install_bower_assets', ['install_bower'], function() {
     .pipe(gulp.dest('public/dependencies'));
 });
 
-gulp.task('test', function(done) {
+gulp.task('test_unit', function(done) {
+  return new Server({
+    configFile: __dirname + '/karma.conf.js'
+  }, done).start();
+});
+
+gulp.task('test_e2e', function(done) {
   return new Server({
     configFile: __dirname + '/karma.conf.js'
   }, done).start();
