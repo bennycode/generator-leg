fa77fdefe3a5d81366ca171c851d70cae29cd8ff
Benny Neugebauer <bn@bennyn.de>
2016-08-13 14:07:41 +0200

    Added JSDoc 3

--- generators/app/templates/src/main/js/Person.js
+++ generators/app/templates/src/main/js/Person.js
@@ -1,7 +1,14 @@
+/**
+ * @param {string} name - The person's name.
+ * @constructor
+ */
 function Person(name) {
   this.name = name;
 }
 
+/**
+ * @returns {string} Greeting sentence
+ */
 Person.prototype.greet = function() {
   return `Hello, my name is ${this.name}.`;
 };
